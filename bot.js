241f11660554357167643f30335850a034241d82
x-pulse <40152105+x-pulse@users.noreply.github.com>
2018-11-04 11:45:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,16 +62,15 @@ client.on('message', message => {
     const command = args.shift().toLowerCase();
 	
 	if(message.content.startsWith(';')) {message.delete().catch(err => {})}
-	let media = [503195594660249611, 501734694245892108, 504120699414249477, 501734734364147740, 490473227106385920, 504120541502898188, 504120979518259201, 501734850668265492];
+	let media = ['503195594660249611', '501734694245892108', '504120699414249477', '501734734364147740', '490473227106385920', '504120541502898188', '504120979518259201', '501734850668265492'];
 	if([message.channel.id].includes(media)) {
-		if(!message.attachments.first()) {
-			message.delete();
-		} else {
-			if(message.author.bot) return;
+		if(message.attachments.first()) {
+			if(message.author.bot) {return;}
 			message.react("👍")
 			message.react("👎")
+		} else {
+			message.delete()
 		}
-		return;
 	}
 	if(message.channel.id === '472968393708929024') {
 		if(message.attachments.first()) {

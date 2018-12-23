d0df81192e52340cfb8513b50bc0d0bf2c7517f5
Z9nn <36812758+cykable@users.noreply.github.com>
2018-12-23 21:09:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ if(message.content.startsWith(p + 'sayem')) {
 }
 if(message.content.startsWith(p + 'say')) {
     let say = message.content.slice((p + 'say').length);
-    message.channel.send(say);
+    message.channel.send(say).catch(err => { })
    }
 	if (message.content.startsWith(admp + `eval`) && (message.author.id === "406343162651738112" || message.author.id === "361951318929309707")) {
 		const code = message.content.split(" ").slice(1).join(" ");
